d7097e6d146c7f6332c3b48b6db38eb76779b1f3
FahadJafar <fahadjaffer123@gmail.com>
2024-09-23 13:59:01 +0500

    Routes Fixed again

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const config = require("./config");
 const authRoutes = require("./routes/auth");
-const path = require("path"); // Import path module
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
+
 const corsOptions = {
   origin: 'https://palito.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -15,11 +16,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-
-app.use(cors(corsOptions));
-
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 mongoose
@@ -27,10 +23,13 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
-app.use("/api/auth", authRoutes);
-router.get("/", (req, res) => {
+
+app.get("/", (req, res) => {
   res.send("Welcome to Fahad Jafars Palito Backend");
 });
+
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server started on port", PORT));
